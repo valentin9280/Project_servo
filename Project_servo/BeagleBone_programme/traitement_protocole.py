eeefbf5046a70da98631c51e0e9400b1da1a3976
valentin9280 <atrax.val@gmail.com>
2016-03-20 22:30:12 +0100

    Modidif traitement protocole

--- Project_servo/BeagleBone_programme/traitement_protocole.py
+++ Project_servo/BeagleBone_programme/traitement_protocole.py
@@ -3,7 +3,29 @@ class traitement_protocole():
 		def __init___(self):
 			pass
 
+			def traitement_trame(data):
+				command = data[0]
+				octet1 = data[1]
+				octet2 = date[2]
 
+				if command == 49:
+					self.MoveMoteur(octet1 , octet2)
+				elif command == 50:
+					if octet1 == 49:
+						self.Laser("OFF")
+					elif octer2 == 50:
+						self.Laser("ON")
+				elif command == 51:
+				     if octet1 == 49:
+				     	self.Dessiner("carre")
+				     elif octet1 == 50
+				     	self.Dessiner("cercle")
+				     elif octet1 == 51
+				     	self.Dessiner("triangle")
+				elif command == 52:
+						self.init()
+
+						
 			def MoveMoteur(self,sens1,sens2):
 				self.MoveMoteur(sens1,sens2)
 
@@ -16,26 +38,6 @@ class traitement_protocole():
 			def init
 				self.init()
 
-			def traitement_trame(data):
-				command = data[0]
-				octet1 = data[1]
-				octet2 = date[2]
 
-				if command == 49:
-					protocole.MoveMoteur(octet1 , octet2)
-				elif command == 50:
-					if octet1 == 49:
-						protocole.Laser("OFF")
-					elif octer2 == 50:
-						protocole.Laser("ON")
-				elif command == 51:
-				     if octet1 == 49:
-				     	protocole.Dessiner("carre")
-				     elif octet1 == 50
-				     	protocole.Dessiner("cercle")
-				     elif octet1 == 51
-				     	protocole.Dessiner("triangle")
-				elif command == 52:
-						protocole.init()
 
 
